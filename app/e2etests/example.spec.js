1854c3a549b20fffe190c1ca2db805d8d7098982
Kirsten Hunter <synedra@gmail.com>
2025-01-23 09:36:20 -0800

    Update example.spec.js

--- app/e2etests/example.spec.js
+++ app/e2etests/example.spec.js
@@ -5,7 +5,7 @@ test('has title', async ({ page }) => {
   await page.goto('http://localhost:8080/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/FusionAuth Express Web/);
+  await expect(page).toHaveTitle(/FusionAuth Start Here Application | Home/);
 });
 
 test('FA has title', async ({ page }) => {
